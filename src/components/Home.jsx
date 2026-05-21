6584ae5e279a62ee1f29f2918ea70d37743f5543
reshmas2026 <reshmasunitha04@gmail.com>
2026-05-21 15:15:24 +0530

    update ui4

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,29 +1,29 @@
 <div className="space-y-6 text-left">
 
   <div>
-    <h1 className="text-6xl font-extrabold text-slate-800 leading-tight">
+    <h1 className="text-6xl font-extrabold text-slate-800">
       Hi, I’m <span className="text-blue-500">Reshma S</span>
     </h1>
 
-    <h2 className="mt-4 text-2xl font-semibold text-sky-600">
+    <h2 className="text-2xl font-semibold text-sky-600">
       Computer Engineering Student
     </h2>
 
-    <p className="mt-3 text-slate-600 leading-relaxed max-w-lg">
+    <p className="text-slate-600 leading-relaxed">
       Passionate about coding, web development & building clean UI.
     </p>
   </div>
 
   <div>
-    <h1 className="text-6xl font-extrabold text-slate-800 leading-tight">
+    <h1 className="text-6xl font-extrabold text-slate-800">
       Hi, I’m <span className="text-blue-500">Reshma S</span>
     </h1>
 
-    <h2 className="mt-4 text-2xl font-semibold text-sky-600">
+    <h2 className="text-2xl font-semibold text-sky-600">
       Computer Engineering Student
     </h2>
 
-    <p className="mt-3 text-slate-600 leading-relaxed max-w-lg">
+    <p className="text-slate-600 leading-relaxed">
       Passionate about coding, web development & building clean UI.
     </p>
   </div>
